dafc50beb24fcf8458a0699cf57665f0c81251e8
renatof4rias <renatof4rias@gmail.com>
2024-07-08 23:26:27 -0300

    build: Estrutura Wrappers

---
+++ com/softelse/WrappersTest.java
@@ -0,0 +1,26 @@
+package com.softelse;
+
+public class WrappersTest {
+
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10f;
+        double doubleP = 10D;
+        char charP = 'W';
+        boolean boolenaP = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10f;
+        Double doubleW = 10D;
+        Character charW = 'W';
+        Boolean boolenaw = false;
+
+
+    }
+}
