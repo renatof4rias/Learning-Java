274cb14a3ede1b06018b604c85f3258d6625105c
renatof4rias <renatof4rias@gmail.com>
2024-09-13 17:40:52 -0300

    feat: domain Local

---
+++ com/softelse/src/assosiacao/domain/Local.java
@@ -0,0 +1,27 @@
+package com.softelse.src.assosiacao.domain;
+
+public class Local {
+    private String secao;
+    private String estante;
+
+    public Local(String secao, String estante) {
+        this.secao = secao;
+        this.estante = estante;
+    }
+
+    public String getSecao() {
+        return secao;
+    }
+
+    public void setSecao(String secao) {
+        this.secao = secao;
+    }
+
+    public String getEstante() {
+        return estante;
+    }
+
+    public void setEstante(String estante) {
+        this.estante = estante;
+    }
+}
