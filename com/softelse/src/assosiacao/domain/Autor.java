dd3a17e8be174bdf50d6ade164e8e5e3ab18c4c5
renatof4rias <renatof4rias@gmail.com>
2024-09-13 17:40:14 -0300

    feat: domain Autor

---
+++ com/softelse/src/assosiacao/domain/Autor.java
@@ -0,0 +1,26 @@
+package com.softelse.src.assosiacao.domain;
+public class Autor {
+    private String nome;
+    private String nacionalidade;
+
+    public Autor(String nome, String nacionalidade) {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+}
