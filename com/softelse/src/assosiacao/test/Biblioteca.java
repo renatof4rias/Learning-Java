f951a253c01e7af64ae59b6aa7873f9275c4dca9
renatof4rias <renatof4rias@gmail.com>
2024-09-13 17:40:28 -0300

    feat: domain Biblioteca

---
+++ com/softelse/src/assosiacao/test/Biblioteca.java
@@ -0,0 +1,23 @@
+package com.softelse.src.assosiacao.test;
+
+import com.softelse.src.assosiacao.domain.Autor;
+import com.softelse.src.assosiacao.domain.Leitor;
+import com.softelse.src.assosiacao.domain.Livro;
+import com.softelse.src.assosiacao.domain.Local;
+
+public class Biblioteca {
+    public static void main(String[] args) {
+
+        Local local = new Local("Pateleira A", "Gaveta 33");
+        Local local1 = new Local("Pateleira A", "Gaveta 34");
+        Autor autor = new Autor("Growth", "Brasileira");
+        Autor autor1 = new Autor("MAX", "Brasileira");
+        Livro livro = new Livro("Jogador N1", 2017, autor, local);
+        Livro livro1 = new Livro("Jogador N2", 2018, autor1, local1);
+
+        Livro[] livros = {livro, livro1};
+        Leitor leitor = new Leitor("Toguro", 34, livros);
+
+        leitor.imprime();
+    }
+}
