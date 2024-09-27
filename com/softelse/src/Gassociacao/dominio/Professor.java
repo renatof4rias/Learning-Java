b66395238c4841f9157d9a68462dace1a951de4e
renatof4rias <renatof4rias@gmail.com>
2024-09-27 15:37:31 -0300

    feat: 00/96 - 03

---
+++ com/softelse/src/Gassociacao/dominio/Professor.java
@@ -0,0 +1,17 @@
+package com.softelse.src.Gassociacao.dominio;
+
+public class Professor {
+    private String nome;
+
+    public Professor(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
