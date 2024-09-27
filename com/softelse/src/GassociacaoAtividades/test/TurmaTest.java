feb1b60a8d0d6a9a41ec4d0dbdfa1aa0b13e4fdf
renatof4rias <renatof4rias@gmail.com>
2024-09-27 18:15:03 -0300

    feat: 00/96 - 15

--- com/softelse/src/GassociacaoAtividades/test/TurmaTest.java
+++ com/softelse/src/GassociacaoAtividades/test/TurmaTest.java
@@ -7,21 +7,18 @@ import com.softelse.src.GassociacaoAtividades.dominio.Turma;
 public class TurmaTest {
     public static void main(String[] args) {
 
-        Nota nota = new Nota(10,9,8);
-        Nota nota1 = new Nota(7,6,5);
-        Nota nota2 = new Nota(4,3,2);
+        Nota nota = new Nota(10, 9, 8);
+        Nota nota1 = new Nota(7, 6, 5);
+        Nota nota2 = new Nota(4, 3, 2);
 
-        Aluno aluno = new Aluno("Renato",nota);
-        Aluno aluno1 = new Aluno("Luiza",nota1);
-        Aluno aluno2 = new Aluno("Helena",nota2);
+        Aluno aluno = new Aluno("Renato", nota);
+        Aluno aluno1 = new Aluno("Luiza", nota1);
+        Aluno aluno2 = new Aluno("Helena", nota2);
 
         Aluno[] alunos = {aluno, aluno1, aluno2};
 
         Turma turma = new Turma("Java", alunos);
-
-
         turma.imprime();
         System.out.println("----------------------------------------------------------");
-
     }
 }
