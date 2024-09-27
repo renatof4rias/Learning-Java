77f8834af3ac7886aa3ca320cbb9bd2df9eff5b5
renatof4rias <renatof4rias@gmail.com>
2024-09-27 18:13:39 -0300

    feat: 00/96 - 10

--- com/softelse/src/Gassociacao/dominio/Time.java
+++ com/softelse/src/Gassociacao/dominio/Time.java
@@ -2,9 +2,31 @@ package com.softelse.src.Gassociacao.dominio;
 
 public class Time {
     private String nome;
+    private Jogador[] jogadores;
+
+    public void imprimir(){
+        System.out.println("Time: " + this.getNome());
+        if(jogadores == null)return;
+        for (Jogador jogador : jogadores) {
+            System.out.println("Jogador: " + jogador.getNome());
+        }
+    }
 
     public Time(String nome) {
         this.nome = nome;
+    }
+
+    public Time(String nome, Jogador[] jogadores) {
+        this.nome = nome;
+        this.jogadores = jogadores;
+    }
+
+    public Jogador[] getJogadores() {
+        return jogadores;
+    }
+
+    public void setJogadores(Jogador[] jogadores) {
+        this.jogadores = jogadores;
     }
 
     public String getNome() {
