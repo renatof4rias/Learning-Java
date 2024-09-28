2d6b7a46e15ea7bed3cf65f4e56568045d43f6dc
renatof4rias <renatof4rias@gmail.com>
2024-09-28 15:25:25 -0300

    feat: 01/96 - 7

---
+++ com/softelse/src/GassociacaoAtividades/dominio/Local.java
@@ -0,0 +1,17 @@
+package com.softelse.src.GassociacaoAtividades.dominio;
+
+public class Local {
+    private String endereco;
+
+    public Local(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
---
+++ com/softelse/src/GassociacaoAtividades/dominio/Professor.java
@@ -0,0 +1,43 @@
+package com.softelse.src.GassociacaoAtividades.dominio;
+
+public class Professor {
+    private String nome;
+    private String especialidade;
+
+    private Seminario[] seminarios;
+
+    public Professor(String nome, String especialidade, Seminario[] seminarios) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+        this.seminarios = seminarios;
+    }
+
+    public Professor(String nome, String especialidade) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+    }
+
+    public Seminario[] getSeminarios() {
+        return seminarios;
+    }
+
+    public void setSeminarios(Seminario[] seminarios) {
+        this.seminarios = seminarios;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+}
