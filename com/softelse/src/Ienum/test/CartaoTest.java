c1d23bf236407e0b165a4bf40e229aebd785b8a1
renatof4rias <renatof4rias@gmail.com>
2024-10-01 17:59:51 -0300

    feat: 04/96 - 2

---
+++ com/softelse/src/Ienum/test/CartaoTest.java
@@ -0,0 +1,24 @@
+package com.softelse.src.Ienum.test;
+
+import com.softelse.src.Ienum.domain.Cartao;
+import com.softelse.src.Ienum.domain.TipoCartao;
+
+public class CartaoTest {
+    public static void main(String[] args) {
+        Cartao cartao = new Cartao("Renato", TipoCartao.CREDITO, 500);
+        Cartao cartao1 = new Cartao("Snow", TipoCartao.DEBITO, 1000);
+
+        System.out.println("---------------------------------");
+        TipoCartao crédito = TipoCartao.getNomeTipoCartao("Crédito");
+        System.out.println(crédito);
+        System.out.println("---------------------------------");
+
+
+//        System.out.println("---------------------------------");
+//        System.out.println(cartao);
+//        System.out.println("---------------------------------");
+//        System.out.println("---------------------------------");
+//        System.out.println(cartao1);
+//        System.out.println("---------------------------------");
+    }
+}
