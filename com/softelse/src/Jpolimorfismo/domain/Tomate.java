9e913d7f7ae02e89cf0eadfc53a1ace54da680f7
renatof4rias <renatof4rias@gmail.com>
2024-10-07 17:38:31 -0300

    feat: Dia 08 - 8

---
+++ com/softelse/src/Jpolimorfismo/domain/Tomate.java
@@ -0,0 +1,15 @@
+package com.softelse.src.Jpolimorfismo.domain;
+
+public class Tomate extends Produto{
+    private static final double IMPOSTO_POR_CENTO = 0.06;
+
+    public Tomate(String nome, double valor) {
+        super(nome, valor);
+    }
+
+    @Override
+    public double calcularImposto() {
+        System.out.println("Calculando imposto do Tomate");
+        return this.valor * IMPOSTO_POR_CENTO;
+    }
+}
