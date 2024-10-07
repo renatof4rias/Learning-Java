ea98e757964d3b1bd630b070e7f30fcfcae682a9
renatof4rias <renatof4rias@gmail.com>
2024-10-07 17:37:54 -0300

    feat: Dia 08 - 1

---
+++ com/softelse/src/Jpolimorfismo/service/CalculadoraImposto.java
@@ -0,0 +1,32 @@
+package com.softelse.src.Jpolimorfismo.service;
+
+import com.softelse.src.Jpolimorfismo.domain.Computador;
+import com.softelse.src.Jpolimorfismo.domain.Produto;
+import com.softelse.src.Jpolimorfismo.domain.Tomate;
+
+public class CalculadoraImposto {
+    public static void calcularImpostoComputador(Computador computador){
+        System.out.println("Relátorio de imposto do computador");
+        double imposto =  computador.calcularImposto();
+        System.out.println("Computador " + computador.getNome());
+        System.out.println("Valor " + computador.getValor());
+        System.out.println("Imposto a ser pago " + imposto);
+    }
+
+    public static void calcularImpostoTomate(Produto tomate){
+        System.out.println("Relátorio de imposto do tomate");
+        double imposto =  tomate.calcularImposto();
+        System.out.println("Tomate " + tomate.getNome());
+        System.out.println("Valor " + tomate.getValor());
+        System.out.println("Imposto a ser pago " + imposto);
+    }
+
+    public static void calcularImposto(Produto produto){
+        System.out.println("Relátorio de imposto");
+        double imposto =  produto.calcularImposto();
+        System.out.println("Produto " + produto.getNome());
+        System.out.println("Valor " + produto.getValor());
+        System.out.println("Imposto a ser pago " + imposto);
+    }
+
+}
