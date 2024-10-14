f44956c0892bb83a23362c9f0733e0bb13b4818a
renatof4rias <renatof4rias@gmail.com>
2024-10-14 17:38:06 -0300

    Aula 114 - Classes Utilitarias - DateFormat

---
+++ com/softelse/src/Ldatas/test/DateTest01.java
@@ -0,0 +1,17 @@
+package com.softelse.src.Ldatas.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+
+    Date date = new Date(1000000000000L);
+    System.out.println( date.toString());
+
+
+
+
+
+
+    }
+}
